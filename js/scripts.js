10ed73c74468eb57ac7b42092a997b997b35b31d
Michael Ortega <michael.ortega@specialcounsel.com>
2021-11-22 14:13:56 -0500

    init

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+* Start Bootstrap - Coming Soon v6.0.5 (https://startbootstrap.com/theme/coming-soon)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+document.getElementById("revealInfo").addEventListener("click", () => {
+	let div = document.getElementById("more-info")
+	let currentStyle = div.style.visibility
+	div.style.visibility = currentStyle === 'hidden' || !currentStyle ? 'visible' : 'hidden'
+});
